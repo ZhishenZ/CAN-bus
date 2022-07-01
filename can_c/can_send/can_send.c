28075a7aa431814864ba724b2ebaf136aad2e95d
ZhishenZ <clemenszhang@gmail.com>
2022-07-01 18:41:48 +0200

    can send

--- can_c/can_send/can_send.c
+++ can_c/can_send/can_send.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -10,87 +11,112 @@
 #include <linux/can/raw.h>
 
 
-int can_send_init(){
+/**
+ * @brief initialization for the can send function for can0
+ *        here struct ifreq ifr and sockaddr_can addr are not passed by pointer
+ *        because they are only used once in this function 
+ * 
+ * @param frame the can frame to be set to zero 
+ * @param s  
+ * @param ifr 
+ * @param addr 
+ * @return int 
+ */
 
+int can_send_init(struct can_frame frame, int *s, struct ifreq ifr, struct sockaddr_can addr){
 
-    return 0;
-}
-
-
-int main()
-{
     int ret;
-    int s, nbytes;
-    struct sockaddr_can addr;
-    struct ifreq ifr;
-    struct can_frame frame;
     memset(&frame, 0, sizeof(struct can_frame));
-
     // close can device before setting the bitrate
     system("sudo ifconfig can0 down");
     // set the bit rate to 500000
 	system("sudo ip link set can0 type can bitrate 500000");
     system("sudo ifconfig can0 up");
-        
+
     /*Create socket*/
-    s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    if (s < 0) {
+    *s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (*s < 0) {
         perror("Create socket PF_CAN failed!");
         return 1;
     }
-    
+
     /*Specify can0 device*/
     strcpy(ifr.ifr_name, "can0");
-    ret = ioctl(s, SIOCGIFINDEX, &ifr);
+    ret = ioctl(*s, SIOCGIFINDEX, &ifr);
     if (ret < 0) {
         perror("ioctl interface index failed!");
         return 1;
     }
-    
+
     /*Bind the socket to can0*/
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-    ret = bind(s, (struct sockaddr *)&addr, sizeof(addr));
+    ret = bind(*s, (struct sockaddr *)&addr, sizeof(addr));
     if (ret < 0) {
         perror("bind failed!");
         return 1;
     }
-    
+
     /*Disable filtering rules,this program only send message do not receive packets */
-    setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+    setsockopt(*s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
 
+    return 0;
+}
+
+
+void close_can_send(int *s){
+
+    /*Close the socket and can0 */
+    close(*s);
+    system("sudo ifconfig can0 down");
+
+}
+
+void send_data( int s, struct can_frame frame, int can_id, uint8_t* data, uint8_t data_len){
+
+    int i;
     /*assembly  message data! */
-    frame.can_id = 0x67D;
-    frame.can_dlc = 8;
-    frame.data[0] = 0x23;
-    frame.data[1] = 0x32;
-    frame.data[2] = 0x47;
-    frame.data[3] = 0x01;
-    frame.data[4] = 0x7B;
-    frame.data[5] = 0x00;
-    frame.data[6] = 0x00;
-    frame.data[7] = 0x00;
-    //if(frame.can_id&CAN_EFF_FLAG==0)
+    frame.can_id = can_id;
+    frame.can_dlc = data_len;
+    for(i=0;i<data_len;i++){
+        frame.data[i] = data[i];
+    }
 
-    // if(!(frame.can_id&CAN_EFF_FLAG))
-	// 	printf("Transmit standard frame!\n");
-	// else
-	// 	printf("Transmit extended frame!\n");
-    // printf("can_id  = 0x%X\r\n", frame.can_id);
-    // printf("can_dlc = %d\r\n", frame.can_dlc);
-    // int i = 0;
-    // for(i = 0; i < 8; i++)
-    //     printf("data[%d] = %d\r\n", i, frame.data[i]);
-    
-    /*Send message out */
-    nbytes = write(s, &frame, sizeof(frame)); 
-    if(nbytes != sizeof(frame)) {
+    i = write(s, &frame, sizeof(frame)); 
+    if(i != sizeof(frame)) {
         printf("Send  frame incompletely!\r\n");
         system("sudo ifconfig can0 down");
     }
+
+}
+
+int main()
+{
+
+    int s;
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+    struct can_frame frame;
     
-    /*Close the socket and can0 */
-    close(s);
-    system("sudo ifconfig can0 down");
+    // initialization
+    if (can_send_init(frame, &s,ifr, addr))
+        return 1;
+
+
+    
+    uint8_t data[ ] = {0x23, 0x32, 0x47, 0x01, 0x7B, 0x00, 0x00, 0x00};
+    uint8_t data_len = sizeof(data)/sizeof(data[0]);
+    int ID = 0x670;
+
+    send_data(s, frame, ID, data, data_len);
+
+    data[0] = 1;
+    data[1] = 2;
+    send_data(s, frame, 1, data , 5);
+
+
+
+    close_can_send(&s);
+
     return 0;
 }
